8396f85de4563d4cbdd3ce081007747c6d344723
ToshnoWor <alekseikoval69@gmail.com>
2023-12-01 17:34:59 +0200

    add new page

---
+++ trainapp/resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+@section('title')About
+@endsection
+@section('main_content')
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal text-body-white">About</h1>
+        <p class="fs-5 text-body-white">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto corporis culpa dicta dolor eaque eligendi esse explicabo, iste non numquam, omnis, praesentium repudiandae vel voluptates voluptatibus? Accusantium eos iste tenetur?</p>
+    </div>
+@endsection
---
+++ trainapp/resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+
+@section('title')Home
+@endsection
+
+@section('main_content')
+    <div class="pricing-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal text-body-white">Home</h1>
+        <p class="fs-5 text-body-white">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+    </div>
+@endsection
